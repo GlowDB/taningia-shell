14255776a954fb3d78a1ee907b44ab70b42006e3
Mahmoud Adel <mahmoud.adel2@gmail.com>
2015-02-18 00:35:38 +0200

    Code refinement..

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 Taningia Shell
 ==============
 A super-user shell which allow controlling multiple hosts in a smart way (Prototype)
+
+**Features:**
+
+* Easy to use interactive mode (default)
+* Easy CLI wizard to save command-groups/host-groups
+* Options to run commands/command-groups non-interactively (e.g compatible with crontab)
+
+**Installation:**
+
+`wget https://raw.githubusercontent.com/mahmoudadel2/taningia-shell/master/taningia-shell.py -O /usr/local/bin/taningia-shell`
+
+`chmod +x /usr/local/bin/taningia-shell`
+
+**Uninstallation:**
+
+`rm -v /usr/local/bin/taningia-shell`
--- taningia-shell.py
+++ taningia-shell.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 __author__ = 'Mahmoud Adel <mahmoud.adel2@gmail.com>'
-__version__ = 0.1
+__version__ = 0.8
 __license__ = "The MIT License (MIT)"
 
 import os
@@ -23,10 +23,10 @@ parser = argparse.ArgumentParser(prog='taningia-shell')
 parser.add_argument('hosts', metavar='HOSTS', help='Target host/hosts')
 rootgroup = parser.add_mutually_exclusive_group()
 groupactions = rootgroup.add_mutually_exclusive_group()
-groupactions.add_argument('-r' ,'--run', metavar='', help='Command/Commands to run on the provided hosts')
-groupactions.add_argument('-g', '--command-group', metavar='', help='Command-group to run on the provided hosts')
+groupactions.add_argument('-r' ,'--run', metavar='', help='Command/Commands to run on the provided hosts "non-interactive"')
+groupactions.add_argument('-g', '--command-group', metavar='', help='Command-group to run on the provided hosts "non-interactive"')
 
-taningiashelldir = '/tmp/taningia-shell'
+taningiashelldir = '/opt/taningia-shell'
 taningiashelltmpdir = taningiashelldir + '/tmp/'
 taningiashellvardir = taningiashelldir + '/var/'
 args = vars(parser.parse_args())
@@ -39,57 +39,54 @@ commandgroupscfg = taningiashelldir + '/etc/' + 'commandgroups.cfg'
 def firsttimeinit():
     if not os.path.exists(taningiashelldir):
         os.mkdir(taningiashelldir)
-        for directory in ('/etc', '/tmp', '/var/'):
+        for directory in ('/etc', '/tmp', '/var'):
             os.mkdir(taningiashelldir + directory)
         open(hostgroupscfg, 'a').close()
         open(commandgroupscfg, 'a').close()
 
-def editfile(editor, file):
+def editfile(editor, rfile):
     if len(hosts) > 1:
         hostsdict = dict()
         index = 1
         for host in hosts:
             hostsdict[index] = host
             index += 1
-        print hostsdict
-        print index
         for key in hostsdict.keys():
-            print str(key) + ') ' + hostsdict[key]
+            print '%s%s) %s%s' % (termcolors.YELLOW, str(key), hostsdict[key], termcolors.END)
         try:
-            hostkey = int(raw_input('Please insert the host to perform the edit action: '))
+            hostkey = int(raw_input('%sPlease insert the host to perform the edit action: %s' % (termcolors.BLUE, termcolors.END)))
             if hostkey in hostsdict.keys():
                 host = hostsdict[hostkey]
-                print host
             else:
-                print 'Nothing to do!'
+                print '%sNothing to do!%s' % (termcolors.RED, termcolors.END)
                 return
         except:
-            print 'Nothing to do!'
+            print '%sNothing to do!%s' % (termcolors.RED, termcolors.END)
             return
     else:
-        host = str(hosts)
-    filename = taningiashelltmpdir + sessionid + '-%s' % file.replace('/', '-')
+        host = hosts[0]
+    filename = taningiashelltmpdir + '-%s' % rfile.replace('/', '-')
     fabric.env.host_string = host
     fabric.env.warn_only = True
-    fabric.get(file, filename)
+    fabric.get(rfile, filename)
     os.system('%s %s' % (editor, filename))
-    fabric.put(filename, file)
+    fabric.put(filename, rfile)
     filemd5sum = hashlib.md5(open(filename).read()).hexdigest()
     shutil.copy(filename, taningiashelltmpdir + '/' + filemd5sum)
-    commands[len(commands) + 1] = (file, filemd5sum)
+    commands[len(commands) + 1] = (rfile, filemd5sum)
 
 def savecmd(cmds):
     for key in cmds.keys():
-        print str(key) + ') ' + str(cmds[key])
-    unwantedcmd = raw_input('Please insert the number of command(s) to ignore separated by comma: ')
-    commandgroupname = raw_input('Please insert a name for the command-group: ')
+        print '%s%s) %s%s' % (termcolors.YELLOW, str(key), str(cmds[key]), termcolors.END)
+    unwantedcmd = raw_input('%sPlease insert the number of command(s) to ignore separated by comma: %s' % (termcolors.BLUE, termcolors.END))
+    commandgroupname = raw_input('%sPlease insert a name for the command-group: %s' % (termcolors.BLUE, termcolors.END))
     if len(unwantedcmd) != 0:
         for unwanted in unwantedcmd.split(','):
             try:
                 if int(unwanted) in cmds.keys():
                     cmds[int(unwanted)] = 'Unwanted'
             except:
-                print 'Nothing to do!'
+                print '%sNothing to do!%s' % (termcolors.RED, termcolors.END)
                 return
     with open(commandgroupscfg, 'a') as f:
         f.writelines('[%s]\n' % commandgroupname)
@@ -99,12 +96,12 @@ def savecmd(cmds):
                 continue
             elif type(cmd) == tuple:
                 f.writelines('  TSPUT:%s,%s\n' % cmd)
-                shutil.copy(taningiashelltmpdir + '/' + cmd[1], taningiashellvardir + '/' + cmd[1])
+                shutil.copy(taningiashelltmpdir + cmd[1], taningiashellvardir + cmd[1])
             else:
                 f.writelines('  %s\n' % cmd)
 
 def savehosts(hosts):
-    hostgroupname = raw_input('Please insert a name for the host-group: ')
+    hostgroupname = raw_input('%sPlease insert a name for the host-group: %s' % (termcolors.BLUE, termcolors.END))
     with open(hostgroupscfg, 'a') as f:
         f.writelines('[%s]\n' % hostgroupname)
         f.writelines('hosts = \n')
@@ -127,8 +124,8 @@ def runcommandgroup(hosts):
     config = ConfigParser.RawConfigParser()
     config.read(commandgroupscfg)
     for section in config.sections():
-        print section
-    cmdgroup = raw_input('Please choose command group to run: ')
+        print '%s%s%s' % (termcolors.YELLOW, section, termcolors.END)
+    cmdgroup = raw_input('%sPlease choose command group to run: %s' % (termcolors.BLUE, termcolors.END))
     if cmdgroup in config.sections():
         for host in hosts:
             fabric.env.warn_only = True
@@ -139,16 +136,20 @@ def runcommandgroup(hosts):
                         if 'TSPUT' in cmd:
                             filedata = str(cmd.split(':')[1]).split(',')
                             with fabric.show('running', 'output'):
-                                fabric.put(taningiashellvardir + '/' + filedata[1], filedata[0])
+                                fabric.put(taningiashellvardir + filedata[1], filedata[0])
                         else:
                             output = fabric.run(cmd)
                             print '''%sOutput from %s:%s
 %s
 ''' % (termcolors.MAGENTA, host, termcolors.END, output)
     else:
-        print 'Nothing to do!'
+        print '%sNothing to do!%s' % (termcolors.RED, termcolors.END)
 
 def connect(hosts):
+    print '''
+%sWelcome to Taningia Shell v%s
+Type 'help' to get a list of Taningia Shell internal commands
+%s''' % (termcolors.BLUE, __version__, termcolors.END)
     try:
         while True:
             cmd = raw_input('%staningia-shell@%s-hosts> %s' % (termcolors.GREEN, len(hosts), termcolors.END))
@@ -156,16 +157,21 @@ def connect(hosts):
                 editor = cmd.split()[0]
                 rfile = cmd.split()[len(cmd.split()) - 1]
                 editfile(editor, rfile)
+            elif cmd == 'help':
+                print '''%s
+                run           Run a saved command-group
+                save          Save a command-group or a host-group
+                %s''' % (termcolors.BLUE, termcolors.END)
             elif cmd == 'run':
                 runcommandgroup(hosts)
             elif cmd == 'save':
-                choice = raw_input('What do want to save:\n1) Commands.\n2) Hosts.\nPlease insert your choice (1,2): ')
+                choice = raw_input('%sWhat do want to save:\n1) Commands.\n2) Hosts.\nPlease insert your choice (1,2): %s' % (termcolors.BLUE, termcolors.END))
                 if choice == '1':
                     savecmd(commands)
                 elif choice == '2':
                     savehosts(hosts)
                 else:
-                    print 'Nothing to do!'
+                    print '%sNothing to do!%s' % (termcolors.RED, termcolors.END)
             else:
                 if len(cmd) != 0:
                     commands[len(commands) + 1] = cmd
